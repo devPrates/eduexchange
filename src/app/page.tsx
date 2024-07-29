3ace354e5cf6833457674ac2b9aa3ac5771db595
Gabriel Prates Bitencourt <devprates@gmail.com>
2024-07-29 00:56:10 -0400

    Header ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+import About from "@/components/About";
+import BackToTop from "@/components/BackToTop";
+import Cta from "@/components/Cta";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Why from "@/components/Why";
 
 export default function Home() {
   return (
-    <main className="">
-      main
+    <main className="max-w-[1920px] bg-white mx-auto relative overflow-hidden">
+      <Header />
+      <Hero />
+      <About />
+      <Why />
+      <Cta />
+      <Footer />
+      <BackToTop />
+      <div className="h-[4000px]"></div>
     </main>
   );
 }
