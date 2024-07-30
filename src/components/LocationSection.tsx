bbc1202bada73cfaa726bb45d7577108c4e87c4a
devprates <devprates@gmail.com>
2024-07-30 12:43:27 -0400

    Date Selection

--- src/components/LocationSection.tsx
+++ src/components/LocationSection.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from "react";
+import React,{ useState } from "react";
 import { Menu, MenuButton, MenuItems } from "@headlessui/react";
 import { MapPin } from 'lucide-react';
 
@@ -11,7 +11,7 @@ const locations = [
     "Campus Três Lagoas - IFMS",
 ]
 export default function LocationSelection() {
-    const [location, setLocation] = useState('select location')
+    const [location, setLocation] = useState('Escolha o Campus')
     return (
         <Menu as='div' className='w-full h-full flex xl:flex-row'>
             <div className="relative flex-1">
@@ -22,12 +22,23 @@ export default function LocationSelection() {
                                 <MapPin className="text-primary"/>
                                 <div className="text-[15px] uppercase font-bold">Selecione o Local</div>
                             </div>
-                            <div className="uppercase font-medium text-[13px] text-gray-500 text-center xl:ml-6 xl:text-left">{location}</div>
+                            <div className="uppercase font-medium text-[13px] text-gray-500 text-center xl:ml-8 xl:text-left">{location}</div>
                         </div>
                     </div>
                 </MenuButton>
 
-                <MenuItems className='dropdown-menu shadow-lg absolute -top-56 xl:top-[90px] left-1/2 xl:left-0 z-10 transform -translate-x-1/2 xl:-translate-x-0 text-sm text-center xl:text-left w-full bg-white max-w-[332px] py-6 rounded-[10px]'>Item de menu</MenuItems>
+                <MenuItems className='dropdown-menu shadow-lg absolute -top-56 xl:top-[90px] left-1/2 xl:left-0 z-10 transform -translate-x-1/2 xl:-translate-x-0 text-sm text-center xl:text-left w-full bg-white max-w-[332px] py-6 rounded-[10px]'>
+                    {locations.map((location, index) => {
+                        return(
+                            <div 
+                                onClick={() => setLocation(location)}
+                                key={index} 
+                                className="cursor-pointer py-4 xl:pl-10 hover:bg-gray-50 text-[13px] uppercase">
+                                    {location}
+                            </div>
+                        )     
+                    })}
+                </MenuItems>
             </div>
         </Menu>
     );
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,13 +3,19 @@
 import { SearchContext } from "@/context/search"
 import { useContext } from "react"
 import LocationSelection from "./LocationSection";
+import DateSelection from "./DateSelection";
 
 export default function Search() {
     const { searchActive } = useContext(SearchContext)
     return (
-        <div className={`${searchActive ? 'bg-white rounded-none xl:h-[80px]' : 'bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]'} hidden xl:block w-full relative shadow-lg`}>
+        <div className={`${
+                searchActive 
+                    ? 'bg-white rounded-none xl:h-[80px]' 
+                    : 'bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]'
+                } hidden xl:block w-full relative shadow-lg`}>
             <div className="xl:h-full flex items-center px-6 xl:px-0">
                 <LocationSelection />
+                <DateSelection />
             </div>
         </div>
     );
