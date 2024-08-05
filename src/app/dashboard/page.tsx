304d53002aa1f4466247e81d0eea331c191a5ecf
Gabriel Prates Bitencourt <devprates@gmail.com>
2024-08-04 23:40:14 -0400

    New routes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+    return (
+        <>
+            <h1 className="container mx-auto">Pagina de login</h1>
+        </>
+    )
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <>
+            <h1 className="container mx-auto">Pagina de login</h1>
+        </>
+    )
+}
