aa8dd77e9b1b82defcc651899fb154d4bffe56fa
devprates <devprates@gmail.com>
2024-08-14 09:02:26 -0400

    new filter of users

--- src/app/(authenticated)/dashboard/users/page.tsx
+++ src/app/(authenticated)/dashboard/users/page.tsx
@@ -1,17 +1,12 @@
 'use client'
 import DashNavbar from "@/components/dashboard/DashNavbar";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { DataTableDemo } from "@/components/tables/TableCursos";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { Button } from "@/components/ui/button";
-import {  CirclePlus, Filter, GraduationCap, School, User2 } from "lucide-react";
-import { useState } from "react";
 import { seed } from "@/dataDashboard";
 import CardCadastro from "@/components/dashboard/cardCadastro";
 
 export default function users() {
-    const [position, setPosition] = useState("top")
     const { cardCadastro } = seed
     
     return (
@@ -50,58 +45,14 @@ export default function users() {
 
             <div>
                 <Tabs defaultValue="account" className="">
-                    <div className="container mt-5 flex justify-between">
+                    <div className="container mt-3 flex justify-between">
                         <TabsList className="bg-gray-500 text-white">
                             <TabsTrigger value="account">Campus</TabsTrigger>
                             <TabsTrigger value="professores">Diretores</TabsTrigger>
                             <TabsTrigger value="turmas">Coordenadores</TabsTrigger>
                         </TabsList>
-                        <div className="flex gap-1">
-                            <DropdownMenu>
-                                <DropdownMenuTrigger asChild>
-                                    <Button className="bg-blue-600 hover:bg-blue-500">
-                                        <Filter className="mr-2 h-4 w-4" />
-                                        Filtrar
-                                    </Button>
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent className="w-56">
-                                    <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-                                        <DropdownMenuRadioItem value="top">A - Z</DropdownMenuRadioItem>
-                                        <DropdownMenuRadioItem value="bottom">Z - A</DropdownMenuRadioItem>
-                                        <DropdownMenuRadioItem value="right">Data</DropdownMenuRadioItem>
-                                    </DropdownMenuRadioGroup>
-                                </DropdownMenuContent>
-                            </DropdownMenu>
-                            <Button disabled className="bg-blue-600 hover:bg-blue-500">Exportar</Button>
-                            <DropdownMenu>
-                                <DropdownMenuTrigger asChild>
-                                    <Button >
-                                        <CirclePlus className="mr-2 h-4 w-4" />
-                                        Cadastrar
-                                    </Button>
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent className="w-56">
-                                    <DropdownMenuLabel>O que deseja cadastrar</DropdownMenuLabel>
-                                    <DropdownMenuSeparator />
-                                    <DropdownMenuGroup>
-                                        <DropdownMenuItem>
-                                            <School className="mr-2 h-4 w-4" />
-                                            <span>Campus</span>
-                                        </DropdownMenuItem>
-                                        <DropdownMenuItem>
-                                            <GraduationCap className="mr-2 h-4 w-4" />
-                                            <span>Diretor</span>
-                                        </DropdownMenuItem>
-                                        <DropdownMenuItem>
-                                            <User2 className="mr-2 h-4 w-4" />
-                                            <span>Coordenador</span>
-                                        </DropdownMenuItem>
-                                    </DropdownMenuGroup>
-                                </DropdownMenuContent>
-                            </DropdownMenu>
-                        </div>
                     </div>
-                    <div className="container mt-6">
+                    <div className="container mt-2">
                         <TabsContent value="account" >
                             <DataTableDemo />
                         </TabsContent>
--- src/components/dashboard/cardCadastro.tsx
+++ src/components/dashboard/cardCadastro.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { cn } from "@/lib/utils"
 import { CardCadastroProps } from "@/types"
 import Link from "next/link"
